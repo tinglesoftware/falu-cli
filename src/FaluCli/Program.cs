b15cb44ebbe1fcea236e482dafae6816a852ff64
Maxwell Weru <mburumaxwell@gmail.com>
2021-08-02 17:34:00 +0300

    Add project files.

---
+++ src/FaluCli/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+
+namespace FaluCli
+{
+    class Program
+    {
+        async static Task<int> Main(string[] args)
+        {
+            // Create a root command with some options
+            var rootCommand = new RootCommand
+            {
+                new Command("evaluations", "Manage evaluations.")
+                {
+                },
+
+                new Command("messages", "Manage messages.")
+                {
+                },
+
+                new Command("payments", "Manage payments.")
+                {
+                },
+
+                new Command("events", "Work with events on Falu.")
+                {
+                },
+
+                new Command("webhooks", "Manage webhooks.")
+                {
+                },
+            };
+
+            rootCommand.Description = "Official CLI tool for Falu.";
+            rootCommand.AddGlobalOption(new[] { "-v", "--verbose" }, "Whether to output verbosely.", () => false);
+
+            var builder = new CommandLineBuilder(rootCommand)
+                .UseHost(_ => Host.CreateDefaultBuilder(args), host =>
+                {
+                    host.ConfigureAppConfiguration((context, builder) =>
+                    {
+                        builder.AddInMemoryCollection(new Dictionary<string, string>
+                        {
+                            ["Logging:LogLevel:Default"] = "Information",
+                            ["Logging:LogLevel:Microsoft"] = "Warning",
+                        });
+                    });
+                })
+                .UseDefaults()
+                .UseTypoCorrections();
+
+            // Parse the incoming args and invoke the handler
+            var parser = builder.Build();
+            return await parser.InvokeAsync(args);
+        }
+    }
+}
