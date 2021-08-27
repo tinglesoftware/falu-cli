568a1d779b61f2b872c49a5c63831e1343f29d26
Maxwell Weru <mburumaxwell@gmail.com>
2021-08-27 16:31:25 +0300

    Added better messages for 401 and 403 errors

--- src/FaluCli/Extensions/CommandLineBuilderExtensions.cs
+++ src/FaluCli/Extensions/CommandLineBuilderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.CommandLine.Invocation;
 using System.CommandLine.IO;
+using System.Net;
 
 namespace System.CommandLine.Builder
 {
@@ -33,24 +34,41 @@ namespace System.CommandLine.Builder
             console.ResetTerminalForegroundColor();
             console.SetTerminalForegroundRed();
 
+            var stderr = console.Error;
+
             if (exception is Falu.Infrastructure.FaluException fe)
             {
                 var error = fe.Error;
                 if (error is not null)
                 {
-                    console.Error.WriteLine($"RequestId: {fe.RequestId}");
-                    console.Error.WriteLine($"TraceId: {fe.TraceId}");
-                    console.Error.WriteLine(error.Title!);
-                    console.Error.WriteLine(error.Detail!);
+                    stderr.WriteLine($"RequestId: {fe.RequestId}");
+                    if (!string.IsNullOrWhiteSpace(fe.TraceId))
+                    {
+                        stderr.WriteLine($"TraceId: {fe.TraceId}");
+                    }
+
+                    stderr.WriteLine($"Code: {error.Title!}");
+                    if (!string.IsNullOrWhiteSpace(error.Detail))
+                    {
+                        stderr.WriteLine(error.Detail);
+                    }
+                }
+                else if (fe.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    stderr.WriteLine("The API key provided cannot authenticate your request.\r\nConfirm you have provided the right value and that it matches any other related values.\r\nFor example, do not use a key that belongs to another workspace or mix live and test mode keys");
+                }
+                else if (fe.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    stderr.WriteLine("The API Key provided or logged in user does not have permissions to perform this operation.\r\nConsult your Falu dashboard to confirm the permissions of the API key or ask your administrator to grant you permissions.");
                 }
                 else
                 {
-                    console.Error.WriteLine(fe.Message);
+                    stderr.WriteLine(fe.Message);
                 }
             }
             else
             {
-                console.Error.WriteLine($"Unhandled exception: {exception}");
+                stderr.WriteLine($"Unhandled exception: {exception}");
             }
 
             console.ResetTerminalForegroundColor();
