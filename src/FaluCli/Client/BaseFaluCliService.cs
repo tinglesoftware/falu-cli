0ad733b14081a96d737aee28a8009911c93dd6f1
Maxwell Weru <mburumaxwell@gmail.com>
2021-08-27 15:01:43 +0300

    Setup handling of events retry using FaluCliClient

---
+++ src/FaluCli/Client/BaseFaluCliService.cs
@@ -0,0 +1,26 @@
+﻿using Falu;
+using Falu.Core;
+using Falu.Infrastructure;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FaluCli.Client
+{
+    internal class BaseFaluCliService : BaseService
+    {
+        public BaseFaluCliService(HttpClient backChannel, FaluClientOptions options) : base(backChannel, options) { }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, RequestOptions? options = null, CancellationToken cancellationToken = default)
+        {
+            if (Options is FaluCliClientOptions fcco)
+            {
+                options ??= new RequestOptions();
+                options.Workspace = fcco.WorkspaceId;
+                options.Live = fcco.Live;
+            }
+
+            return base.SendAsync(request, options, cancellationToken);
+        }
+    }
+}
