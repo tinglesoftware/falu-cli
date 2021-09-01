33b2b19e00ea5eebece69ae3c2bd32d527c91646
Maxwell Weru <mburumaxwell@gmail.com>
2021-09-01 19:18:14 +0300

    React to changes in FaluSdk

--- src/FaluCli/Client/BaseFaluCliService.cs
+++ src/FaluCli/Client/BaseFaluCliService.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace FaluCli.Client
 {
+    // TODO: find another way to work with FaluCliClientOptions
     internal class BaseFaluCliService : BaseService
     {
         public BaseFaluCliService(HttpClient backChannel, FaluClientOptions options) : base(backChannel, options) { }
 
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, RequestOptions? options = null, CancellationToken cancellationToken = default)
+        protected override Task<HttpResponseMessage> RequestCoreAsync(HttpRequestMessage request, RequestOptions? options = null, CancellationToken cancellationToken = default)
         {
             if (Options is FaluCliClientOptions fcco)
             {
@@ -20,7 +21,7 @@ namespace FaluCli.Client
                 options.Live = fcco.Live;
             }
 
-            return base.SendAsync(request, options, cancellationToken);
+            return base.RequestCoreAsync(request, options, cancellationToken);
         }
     }
 }
