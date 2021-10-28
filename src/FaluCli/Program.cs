3bd5606fa99f3caa2fcefda0c4325d708fd99b87
Maxwell Weru <mburumaxwell@gmail.com>
2021-10-28 11:06:07 +0300

    Added configuration for the formatter

--- src/FaluCli/Program.cs
+++ src/FaluCli/Program.cs
@@ -61,9 +61,16 @@ namespace FaluCli
                         {
                             ["Logging:LogLevel:Default"] = "Information",
                             ["Logging:LogLevel:Microsoft"] = "Warning",
+
                             // See https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-5.0#logging
                             ["Logging:LogLevel:System.Net.Http.HttpClient"] = "None", // removes all we do not need
                             ["Logging:LogLevel:System.Net.Http.HttpClient.FaluCliClient.ClientHandler"] = verbose ? "Trace" : "Warning", // add the one we need
+
+                            ["Logging:Console:FormatterName"] = "Falu",
+                            ["Logging:Console:FormatterOptions:SingleLine"] = "True",
+                            ["Logging:Console:FormatterOptions:TimestampFormat"] = "HH:mm:ss ",
+                            ["Logging:Console:FormatterOptions:IncludeCategory"] = "False",
+                            ["Logging:Console:FormatterOptions:IncludeEventId"] = "False",
                         });
                     });
 
