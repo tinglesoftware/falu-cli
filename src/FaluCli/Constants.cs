0c879f7aff3591eca9c9a7c349b226e8bc1131a3
Maxwell Weru <mburumaxwell@gmail.com>
2022-04-08 16:04:15 +0300

    Use api scope and not its resource

--- src/FaluCli/Constants.cs
+++ src/FaluCli/Constants.cs
@@ -6,13 +6,13 @@ internal class Constants
     public const string RepositoryName = "falu-cli";
 
     public const string Authority = "https://login.falu.io";
-    public const string ScopeApi = "https://api.falu.io/";
+    public const string ScopeApi = "api";
     public const string ClientId = "device";
-    public static readonly ICollection<string> ScopesList =new HashSet<string>
+    public static readonly ICollection<string> ScopesList = new HashSet<string>
     {
         IdentityModel.OidcConstants.StandardScopes.OpenId,
         IdentityModel.OidcConstants.StandardScopes.OfflineAccess,
-        ScopeApi
+        ScopeApi,
     };
     public static readonly string Scopes = string.Join(" ", ScopesList);
 }
