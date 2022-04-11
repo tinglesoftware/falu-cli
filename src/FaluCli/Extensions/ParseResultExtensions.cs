52c36daa2b3c00013ca7464b49ccad0fb58cc46a
Maxwell Weru <mburumaxwell@gmail.com>
2022-04-11 12:04:15 +0300

    Added extensions for ParseResult to work with the library updates

---
+++ src/FaluCli/Extensions/ParseResultExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.CommandLine.Parsing;
+
+namespace System.CommandLine;
+
+/// <summary>Extensions for <see cref="ParseResult"/>.</summary>
+internal static class ParseResultExtensions
+{
+    // These extensions exist here as a workaround resuling from changes in beta2
+    // A better way of binding should be sort out
+    public static T? ValueForOption<T>(this ParseResult result, string alias)
+    {
+        if (string.IsNullOrWhiteSpace(alias))
+        {
+            throw new ArgumentException($"'{nameof(alias)}' cannot be null or whitespace.", nameof(alias));
+        }
+
+        var optionResult = result.CommandResult.Children.OfType<OptionResult>().FirstOrDefault(or => or.Option.Aliases.Contains(alias));
+        return optionResult is not null ? optionResult.GetValueOrDefault<T>() : default;
+    }
+
+    public static T? ValueForArgument<T>(this ParseResult result, string alias)
+    {
+        if (string.IsNullOrWhiteSpace(alias))
+        {
+            throw new ArgumentException($"'{nameof(alias)}' cannot be null or whitespace.", nameof(alias));
+        }
+
+        var argumentResult = result.CommandResult.Children.OfType<ArgumentResult>().FirstOrDefault(ar => ar.Argument.Name.Equals(alias));
+        return argumentResult is not null ? argumentResult.GetValueOrDefault<T>() : default;
+    }
+}
