92f0b26cdc4944114d77feba5c06e5748fb248fd
Maxwell Weru <mburumaxwell@gmail.com>
2022-04-11 10:23:17 +0300

    Added logout command

---
+++ src/FaluCli/Commands/Login/LogoutCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Falu.Config;
+
+namespace Falu.Commands.Login;
+
+internal class LogoutCommandHandler : ICommandHandler
+{
+    private readonly IConfigValuesProvider configValuesProvider;
+    private readonly ILogger logger;
+
+    public LogoutCommandHandler(IConfigValuesProvider configValuesProvider, ILogger<LogoutCommandHandler> logger)
+    {
+        this.configValuesProvider = configValuesProvider ?? throw new ArgumentNullException(nameof(configValuesProvider));
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task<int> InvokeAsync(InvocationContext context)
+    {
+        var cancellationToken = context.GetCancellationToken();
+
+        // clear the authentication information and save
+        var values = await configValuesProvider.GetConfigValuesAsync(cancellationToken);
+        values.Authentication = null;
+        await configValuesProvider.SaveConfigValuesAsync(cancellationToken);
+        logger.LogInformation("Authentication information cleared.");
+
+        return 0;
+    }
+}
