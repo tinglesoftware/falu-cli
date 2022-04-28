9b31f59e505097c5d2a4590e782f076402537563
Maxwell Weru <mburumaxwell@gmail.com>
2022-04-28 11:10:23 +0300

    Move global options for workspace id, live mode and api key to WorkspacedCommand

--- src/FaluCli/WorkspacedCommand.cs
+++ src/FaluCli/WorkspacedCommand.cs
@@ -4,5 +4,15 @@ public class WorkspacedCommand : Command
 {
     public WorkspacedCommand(string name, string? description = null) : base(name, description)
     {
+        this.AddGlobalOption(aliases: new[] { "--apikey", },
+                             description: "The API key to use for the command. Required it not logged in or when accessing another workspace. Looks like: sk_test_LdVyn0upN...",
+                             format: Constants.ApiKeyFormat);
+
+        this.AddGlobalOption(aliases: new[] { "--workspace", },
+                             description: "The identifier of the workspace being accessed. Required when login is by user account. Example: wksp_610010be9228355f14ce6e08",
+                             format: Constants.WorkspaceIdFormat);
+
+        this.AddGlobalOption<bool>(aliases: new[] { "--live", },
+                                   description: "Whether the entity resides in live mode or not. Required when login is by user account.");
     }
 }
