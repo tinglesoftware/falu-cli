42d7028daf8c5f6ae363fa523a27006aeec2ace6
Maxwell Weru <mburumaxwell@gmail.com>
2022-05-05 17:02:27 +0300

    Set retries default to 0

--- src/FaluCli/Config/ConfigValues.cs
+++ src/FaluCli/Config/ConfigValues.cs
@@ -2,7 +2,7 @@
 
 internal record ConfigValues
 {
-    public int Retries { get; set; } = 2;
+    public int Retries { get; set; } = 0;
     public string? DefaultWorkspaceId { get; set; }
     public bool DefaultLiveMode { get; set; }
     public AuthenticationTokenConfigData? Authentication { get; set; }
