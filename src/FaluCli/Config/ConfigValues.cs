3e55e5372d5030854cfd25a19aba1dc5e88bb2c2
Maxwell Weru <mburumaxwell@gmail.com>
2022-05-05 16:19:19 +0300

    Added Retries to ConfigValues

--- src/FaluCli/Config/ConfigValues.cs
+++ src/FaluCli/Config/ConfigValues.cs
@@ -2,6 +2,7 @@
 
 internal record ConfigValues
 {
+    public int Retries { get; set; } = 2;
     public string? DefaultWorkspaceId { get; set; }
     public bool DefaultLiveMode { get; set; }
     public AuthenticationTokenConfigData? Authentication { get; set; }
