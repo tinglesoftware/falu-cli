27102afa96a5e9840d5aa2af7236b5d7fed41d2b
Maxwell Weru <mburumaxwell@gmail.com>
2022-05-05 17:01:31 +0300

    Configure options for the client to ensure Retries are set

---
+++ src/FaluCli/Client/FaluClientConfigureOptions.cs
@@ -0,0 +1,20 @@
+﻿using Falu.Config;
+using Microsoft.Extensions.Options;
+
+namespace Falu.Client;
+
+internal class FaluClientConfigureOptions : IConfigureOptions<FaluClientOptions>
+{
+    private readonly IConfigValuesProvider configValuesProvider;
+
+    public FaluClientConfigureOptions(IConfigValuesProvider configValuesProvider)
+    {
+        this.configValuesProvider = configValuesProvider ?? throw new ArgumentNullException(nameof(configValuesProvider));
+    }
+
+    public void Configure(FaluClientOptions options)
+    {
+        var values = configValuesProvider.GetConfigValuesAsync().GetAwaiter().GetResult();
+        options.Retries = values.Retries;
+    }
+}
